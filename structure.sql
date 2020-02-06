bb89950fb00eb30f3e42bff83d295dbcd28b9ba8
Christoph Morrison <post@christoph-jeschke.de>
2020-02-06 20:49:54 +0100

    [HAB-2601] fixed missing args in build_packet_callback

---
+++ structure.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `probe_requests` (
+  `id` bigint(11) unsigned NOT NULL AUTO_INCREMENT,
+  `macaddress` varchar(255) DEFAULT NULL,
+  `make` varchar(255) DEFAULT NULL,
+  `rssi` varchar(255) DEFAULT NULL,
+  `ssid` varchar(255) DEFAULT NULL,
+  `time` timestamp NULL DEFAULT NULL,
+  `uppercaseSSID` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
